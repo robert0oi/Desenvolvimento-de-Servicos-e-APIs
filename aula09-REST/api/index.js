5a54592b0381875fd28dabfbdb353dc9d70c0db9
robert0oi <robertodesouzacamillo@gmail.com>
2025-05-06 11:36:46 -0300

    Terminando Aula10

--- aula09-REST/api/index.js
+++ aula09-REST/api/index.js
@@ -33,19 +33,19 @@ app.get("/product" , (req, res, next) => {
 app.get("/product/:idProd" , (req, res, next) => {
     const id = req.params
     conn("produto")
-        .where("id", id)
-        .first()
-        .then((dados) => {
-            if(!dados){
-                return next(errors(404, "Produto não encontrado."))
-            }
+    .where("id", id)
+    .first()
+    .then((dados) => {
+        if(!dados){
+            return next(errors(404, "Produto não encontrado."))
+        }
             res.json(dados)
         })
         .catch(next)
-})
-
-app.post( "/product" , (req, res, next) => {
-    conn("produto")
+    })
+    
+    app.post( "/product" , (req, res, next) => {
+        conn("produto")
         .insert(req.body)
         .then(dados => {
             if(!dados){
@@ -57,11 +57,42 @@ app.post( "/product" , (req, res, next) => {
             })
         })
         .catch(next)
-})
-
-
-
-
-app.listen(PORT, () => {
-    console.log(`Loja executando em: http://localhost:${PORT}`)
-})
+    })
+    
+    app.put( "/product/:idProd" , (req, res, next) => {
+        const id = req.params.idProd
+        conn("produto")
+        .where("id", id)
+        .update(req.body)
+        .then(dados => {
+            if(!dados){
+                return next(errors(400, "Produto não encontrado.")) 
+            }
+            res.status(200).json({
+                resposta: "Produto editado.",
+                retornoBanco: dados[0]
+            })
+        })
+        .catch(next)
+    })
+    
+    app.delete( "/product/:idProd" , (req, res, next) => {
+        const id = req.params.idProd
+        conn("produto")
+        .where("id", id)
+        .delete()
+        .then(dados => {
+            if(!dados){
+                return next(errors(400, "Produto não encontrado.")) 
+            }
+            res.status(200).json({
+                resposta: "Produto excluído.",
+                retornoBanco: dados[0]
+            })
+        })
+        .catch(next)
+    })    
+    
+    app.listen(PORT, () => {
+        console.log(`Loja executando em: http://localhost:${PORT}`)
+    })
